da9c5827be22f9c50ee95c48044fc63f6780c6cf
Michael <michael@Patricks-MacBook-Pro.local>
2024-11-26 14:37:12 -0600

    Add training script

---
+++ train_rc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --time=1-00:00:00
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --account=blanca-curc-gpu
+#SBATCH --out=log.%j.out
+#SBATCH --error=log.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=michael.ginn@colorado.edu
+
+module purge
+module load gcc/11.2.0
+module load mambaforge
+mamba activate coherence-tuning
+
+cd /projects/$USER/coherence-tuning
+python tune_model.py --model_key openai-community/gpt2
