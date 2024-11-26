c7bf66f5a26dff6ed7064839fa21a8e43253061a
Michael <michael@Patricks-MacBook-Pro.local>
2024-11-26 13:27:34 -0600

    Update dataset and reqs

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -10,22 +10,21 @@ data = datasets.load_dataset(
 data = cast(datasets.DatasetDict, data)
 
 # Make splits
-data = data["train"].train_test_split(test_size=0.25)
-eval_test_splits = data["test"].train_test_split(test_size=0.5)
+data = data["train"].train_test_split(test_size=0.25, seed=0)
+eval_test_splits = data["test"].train_test_split(test_size=0.5, seed=0)
 data["eval"] = eval_test_splits["train"]
 data["test"] = eval_test_splits["test"]
 
 
 # Format our data with a "prefix" and "chosen"/"rejected" final sentences, for DPO
 def process(row):
-    row["prefix"] = " ".join(row[f"InputSentence{n}"] for n in range(1, 5))
+    row["prompt"] = " ".join(row[f"InputSentence{n}"] for n in range(1, 5))
     if row["AnswerRightEnding"] == 1:
         row["chosen"] = row["RandomFifthSentenceQuiz1"]
         row["rejected"] = row["RandomFifthSentenceQuiz2"]
     else:
         row["chosen"] = row["RandomFifthSentenceQuiz2"]
         row["rejected"] = row["RandomFifthSentenceQuiz1"]
-    row["id"] = row["InputStoryid"]
     return row
 
 
